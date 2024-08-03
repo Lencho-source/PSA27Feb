4b61eecc23687289d575238d86e394bc04cd211b
Vijay Mandal <vijaym385@gmail.com>
2024-08-03 20:18:47 +0530

    Defect fiexed-3852

--- src/main/java/com/git_example/A.java
+++ src/main/java/com/git_example/A.java
@@ -1,4 +1,12 @@
 package com.git_example;
 
 public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        A a1= new A();
+        a1.test();
+    }
+
+    private void test() {
+    }
 }
--- src/main/java/com/git_example/B.java
+++ src/main/java/com/git_example/B.java
@@ -1,4 +1,11 @@
 package com.git_example;
 
+
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+
 public class B {
+    public void test(){
+        System.out.println("Learning Java fullStack learning");
+    }
+
 }
